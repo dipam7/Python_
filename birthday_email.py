79abb4ceb6b7b095a4b2143c73b40e6f5f685bc4
dipam <dipam44@gmail.com>
2017-07-18 14:15:07 +0530

    Add functions to the birthday_email

--- birthday_email.py
+++ birthday_email.py
@@ -17,48 +17,55 @@ fw = open(BIRTHDAY_FILE, 'r')
 file_contents = fw.read()
 file_contents = file_contents.split('\n')
 
-for element in file_contents:
-	# ignore whitespace
-	if element == '':
-		continue
+
+def sort_birthdays():
+	for element in file_contents:
+		# ignore whitespace
+		if element == '':
+			continue
 	
-	# if the month is seen for the first time
-	# initialise the month as a key
-	if element.upper() in MONTHS:
-		temp_month = element
-		birthdays_dict[element] = []
+		# if the month is seen for the first time
+		# initialise the month as a key
+		if element.upper() in MONTHS:
+			temp_month = element
+			birthdays_dict[element] = []
 	
-	# else add the date, person's name and email
-	# in the month's list of tuples
-	else:
-		# split the string at ':'
-		# and strip the string of whitespaces
-		my_tuple = [x.strip() for x in element.split(':')]
-		birthdays_dict[temp_month].append(tuple(my_tuple)) 
+		# else add the date, person's name and email
+		# in the month's list of tuples
+		else:
+			# split the string at ':'
+			# and strip the string of whitespaces
+			my_tuple = [x.strip() for x in element.split(':')]
+			birthdays_dict[temp_month].append(tuple(my_tuple)) 
 		
+
+def check_for_birthday():
+	now = datetime.datetime.now()
+	current_month = MONTHS[now.month - 1]
+	current_day = now.day
+
+	print('The current month is ' + current_month)
+
+	# list of everyone who has a birthday this month
+	birthdays_in_current_month = birthdays_dict[current_month.title()]
+	# print('The following people have birthdays this month ' + str(birthdays_in_current_month))
+
+	has_birthday = 0
+	# find if anyone has a birthday today
+	for element in birthdays_in_current_month:
+		if(int(element[0]) == current_day):
+			print('Today\'s date is ' + element[0] + 'th' + ' ' + current_month)
+			print(element[1].upper() + '!!!!!!!' + ' has a birthday today')
+			print('Email him/her on ' + element[2])
+			has_birthday = 1
+			
+	if has_birthday == 0:
+		print('Nobody was born today :( ')
 		
+
+		
+sort_birthdays()
+check_for_birthday()		
+
 # check the complete dict
 print(birthdays_dict)
-
-now = datetime.datetime.now()
-current_month = MONTHS[now.month - 1]
-current_day = now.day
-
-
-print()
-print()
-print()
-
-
-print('The current month is ' + current_month)
-
-# list of everyone who has a birthday this month
-birthdays_in_current_month = birthdays_dict[current_month.title()]
-# print('The following people have birthdays this month ' + str(birthdays_in_current_month))
-
-# find if anyone has a birthday today
-for element in birthdays_in_current_month:
-	if(int(element[0]) == current_day):
-		print('Today\'s date is ' + element[0] + 'th' + ' ' + current_month)
-		print(element[1].upper() + '!!!!!!!' + ' has a birthday today')
-		print('Email him/her on ' + element[2])
